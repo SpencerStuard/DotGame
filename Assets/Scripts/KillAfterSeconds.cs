ddff741133493affbb1ad9ce73e46b1403385557
stuas014 <Spencer.Stuard@disney.com>
2015-01-28 17:28:43 -0800

    adding kill after seconds and finishing up the main menu flow... moving onto the saga map.

---
+++ Assets/Scripts/KillAfterSeconds.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillAfterSeconds : MonoBehaviour {
+
+	float TimeToKill = Mathf.Infinity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > TimeToKill)
+		{
+			Destroy(gameObject);
+		}
+	
+	}
+
+	public void KillMeAfterSeconds (float f)
+	{
+		TimeToKill = Time.time + f;
+	}
+}
